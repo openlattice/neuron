3d82cbf611ca581a032f61b89ea60800787aecf7
Matthew Tamayo-Rios <matthew@openlattice.com>
2022-04-04 05:17:19 -0400

    Update web socket config.

--- src/main/java/com/openlattice/neuron/configuration/WebSocketConfig.java
+++ src/main/java/com/openlattice/neuron/configuration/WebSocketConfig.java
@@ -27,8 +27,6 @@ import static com.openlattice.neuron.constants.WebSocketConstants.DEFAULT_BROKER
 import com.openlattice.authorization.AuthorizationManager;
 import com.openlattice.neuron.configuration.interceptors.AclKeyChannelInterceptor;
 import javax.inject.Inject;
-import org.eclipse.jetty.websocket.api.WebSocketBehavior;
-import org.eclipse.jetty.websocket.api.WebSocketPolicy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
@@ -71,12 +69,6 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Bean
     public DefaultHandshakeHandler handshakeHandler() {
-
-        WebSocketPolicy policy = new WebSocketPolicy( WebSocketBehavior.SERVER );
-        policy.setInputBufferSize( 8192 );
-        policy.setIdleTimeout( 600000 );
-
-        return new DefaultHandshakeHandler( new JettyRequestUpgradeStrategy( policy ) );
+        return new DefaultHandshakeHandler( new JettyRequestUpgradeStrategy(  ) );
     }
-
 }
